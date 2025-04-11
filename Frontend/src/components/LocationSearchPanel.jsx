f2a393c8d15023d2ab6d8c5987eabd1fc4a704a8
pardeep-pathania <pathanianikhil145@gmail.com>
2025-04-11 14:23:34 +0530

    create vehicle and a address selection field

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const LocationSearchPanel = (props) => {
+
+  console.log(props)
+  // sample array for locations
+
+  const locations = [
+    "24B, Near Kapoor's Cafe, Sheryians Academy, Chandigarh",
+    "20A, Near Malhotra's Cafe, Isotech Academy, Punjab",
+    "32C, Near lovely's Sweets, BigBulls Academy, Jalandar",
+    "40B, Near Pathania's Cafe, Rajan Academy, Jammu",
+  ]
+
+
+  return (
+    <div>
+      {/* this is just a sample data */}
+
+
+    {
+      locations.map(function(elem){
+        return <div onClick={()=>{
+          props.setVehiclePanel(true)
+        }} className='flex gap-4 border-2 border-gray-200 active:border-black py-2 px-3 rounded-xl items-center my-2 justify-start'>
+        <h3 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h3>
+        <h4 className='font-medium'>{elem}</h4>
+    </div>
+      })
+    }
+
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,16 +4,21 @@ import {useGSAP} from '@gsap/react'
 
 import gsap from'gsap';
 import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
 
-  const [panelOpen, setPanelOpen] = useState(false)
+  const [panelOpen, setPanelOpen] = useState(false);
+  const vehiclePanelRef = useRef(null)
 
   const panelRef = useRef(null)
 
   const panelCloseRef = useRef(null)
+
+  const [vehiclePanel, setVehiclePanel] = useState(false)
+
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -23,6 +28,7 @@ const Home = () => {
     if(panelOpen){
       gsap.to(panelRef.current,{
         height:'70%',
+        padding:24
         // opacity:1
       })
       gsap.to(panelCloseRef.current,{
@@ -32,6 +38,7 @@ const Home = () => {
     }else{
       gsap.to(panelRef.current,{
         height:'0',
+        padding:0
         // opacity:0
       })
       gsap.to(panelCloseRef.current,{
@@ -39,6 +46,18 @@ const Home = () => {
       })
     }
   },[panelOpen])
+
+  useGSAP(function(){
+    if(vehiclePanel){
+      gsap.to(vehiclePanelRef.current,{
+        transform: 'translateY(0)'
+      })
+    }else{
+      gsap.to(vehiclePanelRef.current,{
+        transform: 'translateY(100%)'
+      })
+    }
+  },[vehiclePanel])
 
 
   return (
@@ -103,8 +122,48 @@ onChange={(e)=>{
           </form>
         </div>
 
-        <div ref={panelRef} className="bg-red-500 h-0"></div>
+        <div ref={panelRef} className="bg-white h-0">
+          <LocationSearchPanel vehiclePanel={vehiclePanel} setVehiclePanel={setVehiclePanel}/>
+        </div>
       </div>
+
+      <div ref={vehiclePanelRef} className="w-full fixed z-10 bottom-0 translate-y-full bg-white px-3 py-8">
+
+        <h3 className="text-2xl font-semibold mb-5">Choose a Vehicle</h3>
+
+        <div className="flex mb-2 p-3 w-full border-2 border-gray-300  active:border-black rounded-xl items-center justify-between">
+          <img className="h-14" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1568134115/assets/6d/354919-18b0-45d0-a151-501ab4c4b114/original/XL.png" alt="" />
+          <div className="ml-2 w-1/2">
+            <h4 className="font-medium text-base">UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+            <h5 className="font-medium text-sm">2 mins away</h5>
+            <p className="font-medium text-xs text-gray-600">Affordable, compact rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">Rs. 180</h2> 
+        </div>
+
+        <div className="flex mb-2 p-3 w-full border-2 border-gray-300  active:border-black rounded-xl items-center justify-between">
+          <img className="h-14" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+          <div className="ml-2 w-1/2">
+            <h4 className="font-medium text-base">Moto<span><i className="ri-user-3-fill"></i>1</span></h4>
+            <h5 className="font-medium text-sm">3 mins away</h5>
+            <p className="font-medium text-xs text-gray-600">Affordable, bike rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">Rs. 65</h2> 
+        </div>
+
+        <div className="flex mb-2 p-3 w-full border-2 border-gray-300 active:border-black rounded-xl items-center justify-between">
+          <img className="h-14" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+          <div className="ml-2 w-1/2">
+            <h4 className="font-medium text-base">UberAuto<span><i className="ri-user-3-fill"></i></span></h4>
+            <h5 className="font-medium text-sm">8 mins away</h5>
+            <p className="font-medium text-xs text-gray-600">Affordable, auto rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">Rs. 118</h2> 
+        </div>
+
+
+      </div>
+
     </div>
   );
 };
